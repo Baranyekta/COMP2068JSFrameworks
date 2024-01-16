5cbe0a3e58da6dcb24aaeaf7c5ea81f6b0b4ca44
Baran Akhavan-Yekta <200529109@student.georgianc.on.ca>
2024-01-15 21:10:26 -0500

    second commit

---
+++ LAB01/lab1.js
@@ -0,0 +1,4 @@
+console.log("Hello world!!");
+
+// in the terminal, type cd "C:\Users\baran\OneDrive\Desktop\M- JavaScript Frameworks COMP2068\COMP2068JSFrameworks\LAB01"
+// then type node lab1.js
